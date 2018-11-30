addecc382cecf6c60f1a54326d726e6167d124de
feijie999 <feijie89@qq.com>
2018-11-30 21:02:56 +0800

    * JieDDDFramework.sln: * Extensions: * JieDDDFramework.Module.Identity: * Data: * JieDDDFramework.Core.csproj:
    * LinqSelectExtensions.cs: 添加Linq扩展
    
    * Models:
    * ApplicationUser.cs:
    * IdentityUserDbContext.cs: 重载IdentityServer的数据库上下文
    
    * IDbContextSeed.cs: 添加数据库上下文初始化数据接口

---
+++ JieDDDFramework.Module.Identity/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace JieDDDFramework.Module.Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
