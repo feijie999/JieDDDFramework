0b718e3b4c4c7b388320b4325facd84325cce111
feijie999 <feijie89@qq.com>
2018-12-01 18:32:04 +0800

    temp

--- JieDDDFramework.Module.Identity/IdentityServerExtensions.cs
+++ JieDDDFramework.Module.Identity/IdentityServerExtensions.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using IdentityServer4.Configuration;
+
 namespace JieDDDFramework.Module.Identity
 {
     public static class IdentityServerExtensions
     {
-       
+       public static IdentityServerBuilder AddDefualtIdentityServerConfig(this IdentityServerBuilder builder)
+        {
+            return builder;
+        }
     }
 }
