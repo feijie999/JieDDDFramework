886f10697a244468b1e4b6a953423dd36f0d4571
feijie <feijie589@gmail.com>
2018-12-02 16:46:49 +0800

    添加Identity模块的配置扩展

--- JieDDDFramework.Module.Identity/IdentityServerExtensions.cs
+++ JieDDDFramework.Module.Identity/IdentityServerExtensions.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using IdentityServer4.AspNetIdentity;
 using IdentityServer4.Configuration;
+using IdentityServer4.EntityFramework.Options;
+using IdentityServer4.Services;
+using JieDDDFramework.Module.Identity.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace JieDDDFramework.Module.Identity
 {
     public static class IdentityServerExtensions
     {
-       public static IdentityServerBuilder AddDefualtIdentityServerConfig(this IdentityServerBuilder builder)
-        {
+       public static IdentityServerBuilder AddDefaultIdentityServerConfig<TApplicationUser>(this IdentityServerBuilder builder,Action<DbContextOptionsBuilder> dbContextOptionsBuilder) where TApplicationUser : ApplicationUser
+       {
+           builder.AddAspNetIdentity<TApplicationUser>()
+               .AddConfigurationStore(options => options.ConfigureDbContext = dbContextOptionsBuilder)
+               .AddOperationalStore(options => options.ConfigureDbContext = dbContextOptionsBuilder)
+               .Services.AddTransient<IProfileService, Services.ProfileService<TApplicationUser>>(); ;
             return builder;
         }
     }
